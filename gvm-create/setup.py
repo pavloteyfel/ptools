73174c7920b996d143ac3c89b8d32c07d6206355
Pasha Taylor <pavloteyfel@gmail.com>
2025-06-12 14:09:10 +0200

    Update setup.py

--- gvm-create/setup.py
+++ gvm-create/setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='gvm-create',
     version='1.0',
-    py_modules=['gvm-create'],
+    py_modules=['gvm_create'],
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'gvm-create = gvm-create:main',
+            'gvm-create = gvm_create:main',
         ],
     },
 )
